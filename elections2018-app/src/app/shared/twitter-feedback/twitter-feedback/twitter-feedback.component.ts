98f85307aa29f0b2dc39f0becca96352788e2d41
Andrés Ruiz Justo <asrj17@gmail.com>
2018-01-25 00:32:51 -0600

    Initial structure

---
+++ elections2018-app/src/app/shared/twitter-feedback/twitter-feedback/twitter-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twitter-feedback',
+  templateUrl: './twitter-feedback.component.html',
+  styleUrls: ['./twitter-feedback.component.css']
+})
+export class TwitterFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
